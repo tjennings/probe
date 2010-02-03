cc50b71ecc957cd31acfcd0d1fb5a982eaf48634
Tyler Jennings <tyle.jennings@gmail.com>
2010-02-03 09:24:40 -0600

    about to re-define the core syntax

---
+++ src/nested_printer.clj
@@ -0,0 +1,32 @@
+(ns nested-printer)
+(use 'clojure.contrib.seq-utils)
+(use 'probe-core)
+
+(def red     "\033[31m")
+(def green   "\033[32m")
+(def brown   "\033[33m")
+(def default "\033[0m")
+
+(defn context-color [context]
+  (if (is-pending context)
+    brown
+    default))
+
+(defmulti nested-printer :type)
+
+(defmethod nested-printer :test
+  ([context] (nested-printer context ""))
+  ([context depth]
+    (if (is-pending context)
+      (str brown depth "- " (:doc context) default "\n")
+      (if (:passed context)
+        (str green depth "- " (:doc context) default "\n")
+        (if-let [assertion (:assertions context)]
+          (str red depth "- " (:doc context) " " (:message assertion) default "\n")
+          (str red depth "- " (:doc context) " " default "\n"))))))
+
+(defmethod nested-printer :context 
+  ([context] (nested-printer context ""))
+  ([context depth]
+    (str (context-color context) depth (:doc context) default "\n"
+      (apply str (doall (map #(nested-printer % (str "  " depth)) (:tests context)))))))
---
+++ src/probe_core.clj
@@ -0,0 +1,17 @@
+(ns probe-core)
+(use 'clojure.contrib.seq-utils)
+
+(defmulti pending-for-type :type)
+
+(defmethod pending-for-type :context [context]
+  (let [tests (:tests context)]
+    (if (empty? tests)
+      true
+      (find-first pending-for-type tests))))
+
+(defmethod pending-for-type :test [test]
+  (empty? (:tests test)))
+
+(defn is-pending [coll]
+  (let [result (find-first pending-for-type (flatten [coll]))]
+    (not (or (nil? result) (empty? result)))))
