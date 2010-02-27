b109949ebfa8ade0a6f5c6caed0ae07acd7e22b6
Jake Scruggs <jake.scruggs@gmail.com>
2010-02-27 10:39:23 -0600

    Jake/Tyler - moving 'migrate' into the probe.util namespace

--- src/probe.clj
+++ src/probe.clj
@@ -1,17 +1,6 @@
 (ns probe)
 (use 'clojure.contrib.seq-utils)
-
-(defn- migrate
-  "Like immigrate, but takes a map of ns -> varnames."
-  [ns-var-map]
-  (doseq [[ns syms] ns-var-map]
-    (require ns)
-    (doseq [sym syms]
-      (let [var (ns-resolve ns sym)]
-        (let [sym (with-meta sym (assoc (meta var) :ns *ns*))]
-          (if (.hasRoot var)
-            (intern *ns* sym (.getRoot var))
-            (intern *ns* sym)))))))
+(use 'probe.util)
  
 (migrate
  {'probe.core '[context it pit run is-pending],
--- src/probe/util.clj
+++ src/probe/util.clj
@@ -6,3 +6,16 @@
    function"
   [pred coll]
   (not (empty? (filter #(pred %) coll))))
+  
+; Blatenly stolen from Stu H's circumspec
+(defn migrate
+  "Like immigrate, but takes a map of ns -> varnames."
+  [ns-var-map]
+  (doseq [[ns syms] ns-var-map]
+    (require ns)
+    (doseq [sym syms]
+      (let [var (ns-resolve ns sym)]
+        (let [sym (with-meta sym (assoc (meta var) :ns *ns*))]
+          (if (.hasRoot var)
+            (intern *ns* sym (.getRoot var))
+            (intern *ns* sym)))))))
